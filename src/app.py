3c46af49929de098062157debb0d2cfe152f5141
JhormanGalindo <j.galindop@uniandes.edu.co>
2025-05-29 14:19:23 -0500

    feat: update environment variable naming and configure JWT manager

--- src/app.py
+++ src/app.py
@@ -1,22 +1,26 @@
-import os
 from flask import Flask, jsonify
 from dotenv import load_dotenv
+from flask_jwt_extended import JWTManager
 
-loaded = load_dotenv('.env.development')
+load_dotenv('.env.development')
 
 from .blueprints.users import users_blueprint
 from .errors.errors import ApiError
 from .models.db import init_db
-
+import os
 
 app = Flask(__name__)
-app.register_blueprint(users_blueprint)
 
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+
+jwt = JWTManager(app)
 init_db(app)
+
+app.register_blueprint(users_blueprint)
 
 @app.errorhandler(ApiError)
 def handle_exception(err):
     response = {
-      'mssg': err.description
+        'mssg': err.description
     }
     return jsonify(response), err.code
