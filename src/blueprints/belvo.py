37aba58f8bc21526f17c5e7736ca7c6b273894c6
JhormanGalindo <j.galindop@uniandes.edu.co>
2025-05-29 20:09:41 -0500

    feat: implement get transactions command and add validation for account ID

--- src/blueprints/belvo.py
+++ src/blueprints/belvo.py
@@ -1,8 +1,9 @@
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from ..commands.get_accounts import GetAccounts
 from ..commands.get_institutions import GetInstitutions
-
+from ..commands.get_transation import GetTransactions
+from ..models.belvos import IdAccountSchema
 
 
 belvos_blueprint = Blueprint("belvos", __name__)
@@ -19,4 +20,13 @@ def get_institutions():
 def get_accounts():
     user_id = get_jwt_identity()  
     command = GetAccounts(user_id).execute()
+    return jsonify(command), 200
+
+@belvos_blueprint.post("/belvos/transactions")
+@jwt_required()
+def get_transactions():
+    json = request.get_json()
+    IdAccountSchema.check(json)
+    user_id = get_jwt_identity()  
+    command = GetTransactions(user_id, json["account_id"]).execute()
     return jsonify(command), 200
--- src/utils/http.py
+++ src/utils/http.py
@@ -63,3 +63,20 @@ def get_accounts_data(link_id):
 
     except requests.exceptions.RequestException:
         raise ApiError()
+    
+def get_transactions_data(account_id, link_id):
+    url = os.getenv("BELVO_PHAT") + f"/api/transactions/?account={account_id}&link={link_id}"
+    auth = (os.getenv("SECRECT_ID"), os.getenv("SECRECT_PASSWORD"))
+    
+    try:
+        response = requests.get(url, auth=auth)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            error = ApiError()
+            error.code = response.status_code
+            error.description = "Error fetching transactions data: " + response.text
+            raise error
+
+    except requests.exceptions.RequestException:
+        raise ApiError()
